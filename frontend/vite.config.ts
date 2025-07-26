99c94341a075205fadf3f62a49cb5e4e6e9781d4
Elis Jackson <elisjackson@icloud.com>
2025-07-26 20:59:45 +0100

    Lars working

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: "build" }
-});
+  server: {
+    port: 5173,
+    allowedHosts: ['.ngrok-free.app']
+  },
+  build: {
+    outDir: "build"
+  }
+})
